af0ce9d68222e2d3efa33d5e001f256589cbc205
lvrico@paterostechnologicalcollege.edu.ph <lvrico@paterostechnologicalcollege.edu.ph>
2026-03-12 00:47:28 +0800

    Update grp2_rico_ex3.py

--- grp2_rico_ex3.py
+++ grp2_rico_ex3.py
@@ -1,1 +1,39 @@
+# My first simple script
+
+# Full name, course, year and section
+print("Hello, I'm Lyka Sophia Rico, BSIT 1")
+
+# Python Variables
+name = "Lyka"
+number = 19
+
+print("Name:", name)
+print("Number:", number)
+
+# Python - Variable Names (group members first names)
+angel = "angel"
+ruzzel = "ruzzel"
+era = "era"
+adrian = "adrian"
+
+print(angel)
+print(ruzzel)
+print(era)
+print(adrian)
+
+# Many Values to Multiple Variables
+x, y, z = 5, 10, 15
+
+print(x)
+print(y)
+print(z)
+
+# One Value to Multiple Variables
+a = b = c = 100
+
+print(a)
+print(b)
+print(c)
+12:34 AM
+You sent
 
